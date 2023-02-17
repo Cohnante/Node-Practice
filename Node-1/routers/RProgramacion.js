1b3772bf34e122a8c907a8e088b621ef5c648b02
cohnante <gcbolaos3@misena.edu.co>
2023-02-16 19:03:58 -0500

    Comenzar a practicar con CRUD

--- Node-1/routers/RProgramacion.js
+++ Node-1/routers/RProgramacion.js
@@ -6,6 +6,7 @@ const {programacion} = require('../db/curso').InforCursos
 
 const routerProgramacion =express.Router();
 app.use("/api/cursos/programacion", routerProgramacion)
+routerProgramacion.use(express.json())
 
 routerProgramacion.get('/',(req,res)=>{
   res.send(JSON.stringify(programacion));
@@ -37,6 +38,51 @@ routerProgramacion.get('/:lenguaje/:nivel',(req,res)=>{
   }
 
   res.send(JSON.stringify(resultados));
+})
+
+routerProgramacion.post('/',(req,res)=>{
+  let BodyNew = req.body;
+  programacion.push(BodyNew)
+  res.send(JSON.stringify(programacion));
+});
+
+routerProgramacion.put('/:id',(req,res)=>{
+  const UpdateBody = req.body;
+  const id = req.params.id;
+
+  const index = programacion.findIndex(curso => curso.id == id)
+
+ if(index>=0){
+  programacion[index]=UpdateBody;
+ } 
+ res.send(JSON.stringify(programacion));
+
+})
+
+routerProgramacion.patch('/:id',(req,res)=>{
+  const InfoBody = req.body;
+  const id = req.params.id;
+
+  const index = programacion.findIndex(curso => curso.id == id)
+
+  if(index>=0){
+    const CursoModi = programacion[index];
+    Object.assign(CursoModi,InfoBody);
+  }
+
+  res.send(JSON.stringify(programacion))
+})
+
+routerProgramacion.delete('/:id',(req,res)=>{
+  const id = req.params.id;
+
+  const Index = programacion.findIndex(curso => curso.id ==id);
+
+  if(Index>=0){
+    programacion.splice(Index,1);
+  }
+
+  res.send(JSON.stringify(programacion))
 })
 
 module.exports.routerProgramacion = routerProgramacion;
